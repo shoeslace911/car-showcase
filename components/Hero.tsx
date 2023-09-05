127437011699b023275f6d0b80975f323847e07c
Sho Fujiwara <shofujiwara505@gmail.com>
2023-09-05 12:39:28 +0900

    layout css, successfully copy and pasted

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import img from "next/image";
+
+const Hero = () => {
+  return (
+    <div className="text-2xl">
+      <h1>HEllo</h1>
+    </div>
+  );
+};
+
+export default Hero;
