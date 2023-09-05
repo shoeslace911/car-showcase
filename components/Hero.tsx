f86dbb97246801041ee98f0ce293c93c77034f2d
Sho Fujiwara <shofujiwara505@gmail.com>
2023-09-05 14:08:41 +0900

    added images

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,16 +6,16 @@ import { CustomButton } from ".";
 const Hero = () => {
   const handleScroll = () => {};
   return (
-    <div className="text-2xl">
-      <div className="flex flex-col pt-36 padding-x">
+    <div>
+      <div className="flex flex-col pt-36 ">
         <h1 className="hero__title">Find, book, or rent a car -- quickly and easily</h1>
         <p className="hero__subtitle">Streamline your car rental experience with our effortless booking process</p>
-        <CustomButton
-          title="Explore Cars"
-          containerStyles="bg-primary-blue text-white rounded-full mt-10"
-          handleClick={handleScroll}
-        />
       </div>
+      <CustomButton
+        title="Explore Cars"
+        containerStyles="bg-primary-blue text-white rounded-full mt-10"
+        handleClick={handleScroll}
+      />
     </div>
   );
 };
