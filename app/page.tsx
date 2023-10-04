76fd1584f54cc95d72513f2d4f5dfab659ac105e
Sho Fujiwara <shofujiwara505@gmail.com>
2023-10-04 13:11:32 +0900

    drop down shows reccomends what you write down in the input box

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
-import { Hero } from "@/components";
+import { CustomFilter, Hero, SearchBar } from "@/components";
+import { useState } from "react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Hero />
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container">
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
+          <p>Explore out cars you might like</p>
+        </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
