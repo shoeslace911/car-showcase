cc5b1f0c4c20c7100c723d5bbb96e6cb110a2d17
Sho Fujiwara <shofujiwara505@gmail.com>
2023-11-03 15:36:11 +0900

    display for price per day done

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -31,13 +31,13 @@ const CarCard = ({ car }: CarCardProps) => {
     <div className="car-card group">
       <div className="car-card__content">
         <h2 className="car-card__content-title">
-          {make}
-          <br />
-          {model}
+          {make} {model}
         </h2>
       </div>
       <p className="flex mt-6 text-[32px] font-extrabold ">
-        <span className="self-start text-[14] font-semibold ">{carRent}</span>
+        <span className="self-start text-[14px] font-semibold">$</span>
+        {carRent}
+        <span className="self-end text-[14px] font-medium ">/day</span>
       </p>
     </div>
   );
