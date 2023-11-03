bede72f8642816625b4019a663f4f570118499f7
Sho Fujiwara <shofujiwara505@gmail.com>
2023-11-03 15:32:19 +0900

    rent calculation via mph and city done

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import React from "react";
 import { CarProps } from "@/types";
+import { calculateCarRent } from "@/utils";
 import { CustomButton } from ".";
 
 interface CarCardProps {
@@ -23,14 +24,21 @@ const CarCard = ({ car }: CarCardProps) => {
     transmission,
     year,
   } = car;
+
+  const carRent = calculateCarRent(city_mpg, year);
+
   return (
     <div className="car-card group">
       <div className="car-card__content">
-        <h2>
+        <h2 className="car-card__content-title">
           {make}
+          <br />
           {model}
         </h2>
       </div>
+      <p className="flex mt-6 text-[32px] font-extrabold ">
+        <span className="self-start text-[14] font-semibold ">{carRent}</span>
+      </p>
     </div>
   );
 };
