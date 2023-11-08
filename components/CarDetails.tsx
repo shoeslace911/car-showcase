79643ea01df6283a004e5da981b5819082bd1ab9
Sho Fujiwara <shofujiwara505@gmail.com>
2023-11-08 14:44:02 +0900

    modal pops out time to design

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -41,9 +41,17 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                   leaveTo="opacity-0 scale-95"
                 >
                   <Dialog.Panel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xsl transition-all flex flex-col gap-5">
-                    <button type="button" onClick={closeModal}>
+                    <button
+                      type="button"
+                      onClick={closeModal}
+                      className="absolute top-2 right-2 z-10 w-fit p-2 bg-primary-blue-100 rounded-full"
+                    >
                       <Image src="/X.png" alt="close" width={20} height={20} className="object-contain" />
                     </button>
+
+                    <div className="flex-1 flex flex-col gap-3">
+                      <div className="relative w-full h-40 bg-pattern bg-cover bg-center rounded-lg"></div>
+                    </div>
                   </Dialog.Panel>
                 </Transition.Child>
               </div>
