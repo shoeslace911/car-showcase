2c543481ad644844667282edbdc7207b889f06da
Sho Fujiwara <shofujiwara505@gmail.com>
2023-11-24 17:00:16 +0900

    details in car cards

--- utils/index.ts
+++ utils/index.ts
@@ -3,7 +3,7 @@ export async function fetchCars() {
     "X-RapidAPI-Key": "3901eccdecmsh1e3d3f88c927c78p19d996jsn32781c993070",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
   };
-  const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla", { headers: headers });
+  const response = await fetch("https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=carrera", { headers: headers });
   //the second parameter in fetch(headers in this case) sends out additional info for your api
 
   const result = await response.json();
